00e14815badc054239d6e0c07562fd5f327328be
abbasbadshah <abbasbadshah603@gmail.com>
2024-06-07 12:20:27 +0530

    update node modules andnext config file

--- next.config.js
+++ next.config.js
@@ -1,15 +1,2 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-              protocol: 'https',
-              hostname: 'www.pexels.com',
-              port: '',
-              pathname: '',
-            },
-          ],
-    }
-}
 
-module.exports = nextConfig
