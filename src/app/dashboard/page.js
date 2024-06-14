108c7c7d6a1b2d2b202bd116e7f01f9677314cb6
abbasbadshah <abbasbadshah603@gmail.com>
2024-06-14 15:53:55 +0530

    remove hookfile

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,40 +1,37 @@
 "use client";
 import { Modal } from "@/components/Modal";
-import {
-  Delete,
-  Edit,
-  ExpandLess,
-  ExpandMore,
-  PersonAdd,
-} from "@mui/icons-material";
-import { useUsersTable } from "./hook/useUserManagement";
+import { useState, useEffect } from "react";
+import Fuse from "fuse.js";
+import { Delete, ExpandLess, ExpandMore, PersonAdd, Edit } from "@mui/icons-material";
 
 export default function Dashboard() {
-  const {
-    userToEdit,
-    setUserToEdit,
-    users,
-    setSearchTerm,
-    subscriptionEndDate,
-    showDropdown,
-    setShowDropdown,
-    showBatchDropdown,
-    setShowBatchDropdown,
-    selectedBatches,
-    selectedRole,
-    showRoleDropdown,
-    setShowRoleDropdown,
-    handleRoleSelection,
-    handleBatchSelection,
-    getUniqueBatches,
-    handleDelete,
-    showModal,
-    setShowModal,
-    filteredUsers,
-  } = useUsersTable();
+  const [userToEdit, setUserToEdit] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [users, setUsers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [subscriptionEndDate, setSubscriptionEndDate] = useState();
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [showBatchDropdown, setShowBatchDropdown] = useState(false);
+  const [selectedBatches, setSelectedBatches] = useState([]);
+  const [selectedRole, setSelectedRole] = useState("");
+  const [showRoleDropdown, setShowRoleDropdown] = useState(false);
+  const [sortOrder, setSortOrder] = useState('asc');
 
-  const toggleDropdown = () => {
-    setShowDropdown(!showDropdown);
+  const handleRoleSelection = (role) => {
+    setSelectedRole(role);
+  };
+
+  const handleBatchSelection = (batch) => {
+    if (selectedBatches.includes(batch)) {
+      setSelectedBatches(selectedBatches.filter((b) => b !== batch));
+    } else {
+      setSelectedBatches([...selectedBatches, batch]);
+    }
+  };
+
+  const getUniqueBatches = () => {
+    const batches = new Set(users.map((user) => user.batch));
+    return Array.from(batches).sort((a, b) => b - a);
   };
 
   const renderDropdownOptions = () => {
@@ -104,6 +101,84 @@ export default function Dashboard() {
         </li>
       </div>
     ) : null;
+  };
+
+  const fetchUsers = async () => {
+    const response = await fetch("/api/users");
+    const data = await response.json();
+    setUsers(data);
+  };
+
+  const getSubscriptionEndDate = async (its) => {
+    const response = await fetch(`/api/fetch-user-subscription/${its}`);
+    const data = await response.json();
+    setSubscriptionEndDate(data.endDate);
+  };
+
+  useEffect(() => {
+    fetchUsers();
+    getSubscriptionEndDate(30406688);
+  }, [showModal]);
+
+  const handleDelete = async (its) => {
+    try {
+      const confirmDelete = window.confirm(
+        "Are you sure you want to delete this user?"
+      );
+      if (confirmDelete) {
+        const response = await fetch(`/api/delete-user/${its}`, {
+          method: "DELETE",
+        });
+
+        if (!response.ok) throw new Error("Deletion failed");
+
+        setUsers(users.filter((user) => user.its !== its));
+      }
+    } catch (error) {
+      console.error("Failed to delete user:", error);
+    }
+  };
+
+  const fuseOptions = {
+    keys: ["name"],
+    includeScore: true,
+    threshold: 0.3,
+  };
+
+  const fuse = new Fuse(users, fuseOptions);
+
+  const sortUsers = (a, b) => {
+    const nameA = a.name.toLowerCase();
+    const nameB = b.name.toLowerCase();
+
+    if (nameA < nameB) {
+      return sortOrder === 'asc' ? -1 : 1;
+    }
+    if (nameA > nameB) {
+      return sortOrder === 'asc' ? 1 : -1;
+    }
+    return 0;
+  };
+  const sortedUsers = [...users].sort(sortUsers);
+
+  const filteredUsers = searchTerm
+  ? sortedUsers.filter(
+      (user) =>
+        (selectedBatches.length === 0 || selectedBatches.includes(user.batch)) &&
+        (selectedRole === '' || user.role === selectedRole) &&
+        (user.its.toString().includes(searchTerm) ||
+          fuse
+            .search(searchTerm)
+            .map((result) => result.item)
+            .includes(user))
+    )
+  : sortedUsers.filter(
+      (user) =>
+        (selectedBatches.length === 0 || selectedBatches.includes(user.batch)) &&
+        (selectedRole === '' || user.role === selectedRole)
+    );
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
   };
 
   return (
@@ -266,7 +341,6 @@ export default function Dashboard() {
                     setShowModal(true);
                   }}
                 />
-
                 <Delete
                   onClick={() => handleDelete(user.its)}
                   className="text-[#ff1a1a] cursor-pointer"
